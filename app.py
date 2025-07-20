32c32ca06cd6fa110f3127c3128fc0c650024e5c
Andrés Ulloa Jaramillo <aeulloa50@gmail.com>
2025-07-19 21:15:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if authentication_status:
         </div>
         """
         # Usar componentes para incrustar el iframe
-        components.html(powerbi_iframe, height=650)
+        components.html(powerbi_iframe, height=850)
 
     render_powerbi_dashboard()
 
